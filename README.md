67b504450dd6ad87d941aa860bebac7ff4b7ad5e
Alexander Apanovich <al29051999@mail.ru>
2021-05-23 02:35:06 +0300

    initial

---
+++ README.md
@@ -0,0 +1,17 @@
+# pi-power-button
+
+Python script to power on/off Raspberry PI 4
+
+Guide for Raspberry PI 4:
+1. Connect button between GPIO 3 (SCL) and ground
+2. git clone https://github.com/AlexanderApanovich/rpi-power-button.git
+3. cd rpi-power-button
+4. sudo ./install
+5. sudo crontab -e  
+Add following string: @reboot python /usr/local/bin/pi_power_button.py &
+
+Uninstallation guide:
+1. sudo crontab -e  
+Remove following string: @reboot python /usr/local/bin/pi_power_button.py &
+2. cd rpi-power-button
+3. sudo ./uninstall
---
+++ pi_power_button.py
@@ -0,0 +1,43 @@
+import logging
+import logging.handlers
+import os
+import subprocess
+import time
+import RPi.GPIO as GPIO
+
+# config
+gpio_pin = 3
+poll_period_sec = 0.1
+delay_before_shutdown_sec = 0.5
+shutdownCommand = "shutdown -h now".split()
+
+# raspberry pi
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(gpio_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# logging
+handler = logging.handlers.WatchedFileHandler("/var/log/pi_power_button/pi_power_button.log")
+formatter = logging.Formatter(logging.BASIC_FORMAT)
+handler.setFormatter(formatter)
+root = logging.getLogger()
+root.setLevel(os.environ.get("LOGLEVEL", "INFO"))
+root.addHandler(handler)
+
+
+def shutdown():
+    try:
+        subprocess.check_output(shutdownCommand)
+    except Exception as e:
+        logging.exception(str(e))
+
+
+def init():
+    while True:
+        time.sleep(poll_period_sec)
+        if GPIO.input(gpio_pin) == False:
+            while GPIO.input(gpio_pin) == False:
+                time.sleep(delay_before_shutdown_sec)
+            shutdown()
+
+
+init()
